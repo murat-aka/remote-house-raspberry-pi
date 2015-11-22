6f643cf1b88f772920b09ceeec677b232ccf57e0
murat-aka <murat-aka@hotmail.com>
2015-11-22 03:25:12 +0000

    update project

--- js/spline.js
+++ js/spline.js
@@ -13,7 +13,100 @@ $(function () {
                 animation: Highcharts.svg, // don't animate in old IE
                 marginRight: 10,
                 events: {
-                    load: function() {
+				
+					load: function(){
+
+
+                        var series = this.series[0];
+                        setInterval(function() {
+						
+						
+						
+						
+						    getLocation();
+  
+  var units = false;
+
+  function getLocation() {
+    $.get("http://ipinfo.io", function(location) {
+      console.log(location);
+      
+      $('.location')
+        .append(location.city + ", ")
+        .append(location.region);
+
+      var units = "imperial"//getUnits(location.country);
+      
+      
+      getWeather(location.loc, units);
+
+      //return weather;
+
+    }, "jsonp");
+
+  }
+
+  function getWeather(loc, units) {
+    lat = loc.split(",")[0].toString();
+    lon = loc.split(",")[1].toString();
+    
+    //lat = 51.5186161;
+    //lon = -0.1493243;
+
+    var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + "&units=" + units +'&APPID=bc93a8c6f91688738e890232a6c24ced';
+
+    console.log(weatherApiUrl);
+
+    $.get(weatherApiUrl, function(weather) {
+      var windDir = convertWindDirection(weather.wind.deg);
+      var temperature = weather.main.temp;
+      var unitLabel;
+
+      //label based in imperial vs metric units
+      if (units === "imperial") {
+        unitLabel = "F";
+      } else {
+        unitLabel = "C";
+      }
+
+      temperature = parseFloat((temperature).toFixed(1));
+	  
+	     var x = (new Date()).getTime(), // current time
+                                y = temperature //Math.random();
+                            series.addPoint([x, y], true, true);
+
+
+    }, "jsonp");
+
+  };
+
+  function convertWindDirection(dir) {
+    var rose = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    var eightPoint = Math.floor(dir / 45);
+    return rose[eightPoint];
+  }
+
+  function getUnits(country) {
+    var imperialCountries = ['US', 'BS', 'BZ', 'KY', 'PW'];
+
+    if (imperialCountries.indexOf(country) === -1) {
+      var units = 'metric';
+    } else {
+      units = 'imperial';
+    }
+
+    console.log(country, units);
+    return units;
+  }
+                         
+                        }, 3000);
+
+
+						
+  
+  }
+					
+				    /*    load: function() {
     
                         // set up the updating of the chart each second
                         var series = this.series[0];
@@ -22,7 +115,7 @@ $(function () {
                                 y = Math.random();
                             series.addPoint([x, y], true, true);
                         }, 1000);
-                    }
+                    }*/
                 }
             },
             title: {
